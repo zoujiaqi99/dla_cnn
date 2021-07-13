980304b92bcb66cbb4c36b0a91e91d3fc487eab4
Jiaqi Zou <zoujq20@mails.tsinghua.edu.cn>
2021-07-13 22:39:54 +0800

    Update defs.py

--- desi/defs.py
+++ desi/defs.py
@@ -4,5 +4,6 @@
 #REST_RANGE = [920, 1214, "TBD"]
 REST_RANGE = [900, 1346, "TBD"]
 kernel = 400 # 600 smooth
+smooth_kernel = 600
 best_v = {'b': 62996, 'r': 44859, 'z': 34720, 'all': 44735}#the best value of rebining for each channel, its unit is m*s^(-1).
 best_v_sv={'b': 51028, 'r': 35849, 'z': 27656}
